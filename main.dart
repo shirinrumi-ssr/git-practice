a96c939bb96c6e42e97073e0da51c53804fe0576
shirinrumi-ssr <s.shirinrumi@gmail.com>
2026-06-20 16:33:15 +0600

    add a if statement

--- main.dart
+++ main.dart
@@ -1,5 +1,11 @@
 void main() {
   String Name = 'Shirin Sultana Rumi';
   print("Name: $Name");
+  print("add your location");
+  
+  if (int i =0; i<6; i++)
+  {
+    print ("number= $i");
+  }
   //print("hello world");
 }
